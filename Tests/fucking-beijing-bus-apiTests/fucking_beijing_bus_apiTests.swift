705d3349d683381bc95d18faeed0607bfe8fee12
leavez <gaojiji@gmail.com>
2018-10-21 00:15:10 +0800

    add UT

--- Tests/fucking-beijing-bus-apiTests/fucking_beijing_bus_apiTests.swift
+++ Tests/fucking-beijing-bus-apiTests/fucking_beijing_bus_apiTests.swift
@@ -19,7 +19,16 @@ final class fucking_beijing_bus_apiTests: XCTestCase {
     func test_getAllLines() {
         itWait { (done) in
             BeijingBusAPI.Static.getAllLines(completion: { (infos) in
-                print(infos)
+                if let value = infos.value {
+                    XCTAssert(value.count > 2000)
+                    let aLine = value[100]
+                    XCTAssert(aLine.ID.count > 0)
+                    XCTAssert(aLine.busNumber.count > 0)
+                    XCTAssert(aLine.departureStationName.count > 0)
+                    XCTAssert(aLine.terminalStationName.count > 0)
+                } else {
+                    XCTFail()
+                }
                 done()
             })
         }
@@ -34,7 +43,20 @@ final class fucking_beijing_bus_apiTests: XCTestCase {
                     (lineID: "404", stationName: "北京航空航天大学", indexInBusLine: 28), // 478
                     (lineID: "1827", stationName: "学知桥北", indexInBusLine: 37)
                 ], completion: { infos in
-                    print(infos)
+                    if let value = infos.value {
+                        if value.count > 0 {
+                            let info = value[0]
+                            XCTAssert(info.ID.count > 0)
+                            XCTAssert(info.gpsUpdatedTime != -1)
+                            XCTAssert(info.currentLocation != Coordinate(longitude: -1, latitude: -1))
+                            XCTAssert(info.nextStation.name.count > 0)
+                            XCTAssert(info.currentStation.distanceRemain != -1)
+                        } else {
+                            print("现在是夜晚，没有车运行的，会返回空。否则有问题")
+                        }
+                    } else {
+                        XCTFail()
+                    }
                     done()
             })
         }
@@ -44,7 +66,20 @@ final class fucking_beijing_bus_apiTests: XCTestCase {
         itWait { (done) in
             // 478 线路相对于"北京航空航天大学"
             BeijingBusAPI.RealTime.getAllBusesStatus(ofLine: "404", referenceStation: 28, completion: { (infos) in
-                print(infos)
+                if let value = infos.value {
+                    if value.count > 0 {
+                        let info = value[0]
+                        XCTAssert(info.ID.count > 0)
+                        XCTAssert(info.gpsUpdatedTime != -1)
+                        XCTAssert(info.currentLocation != Coordinate(longitude: -1, latitude: -1))
+                        XCTAssert(info.nextStation.name.count > 0)
+                        XCTAssert(info.currentStation.distanceRemain != -1)
+                    } else {
+                        print("现在是夜晚，没有车运行的，会返回空。否则有问题")
+                    }
+                } else {
+                    XCTFail()
+                }
                 done()
             })
         }
@@ -53,7 +88,20 @@ final class fucking_beijing_bus_apiTests: XCTestCase {
     func test_getLineDetail() {
         itWait { (done) in
             BeijingBusAPI.Static.getLineDetail(ofLine: "404", completion: { (result) in
-                print(result)
+                if let v = result.value as? LineDetail {
+                    XCTAssert(v.ID.count > 0)
+                    XCTAssert(v.busNumber.count > 0)
+                    XCTAssert(v.departureStationName.count > 0)
+                    XCTAssert(v.terminalStationName.count > 0)
+                    XCTAssert(v.coords.count > 0)
+                    XCTAssert(v.stations.count > 0)
+                    let station = v.stations[0]
+                    XCTAssert(station.index > 0)
+                    XCTAssert(station.name.count > 0)
+                    XCTAssert(station.location != Coordinate(longitude: -1, latitude: -1))
+                } else {
+                    XCTFail()
+                }
                 done()
             })
         }
